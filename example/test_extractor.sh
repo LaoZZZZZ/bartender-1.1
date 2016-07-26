0387e8c07b0a96ce0940042bfb201fbfb9265b41
HAHA <luzhao1986@gmail.com>
2016-07-26 14:21:44 -0400

    Update test_extractor.sh

--- example/test_extractor.sh
+++ example/test_extractor.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-bartender_extractor_com -f 2M_test.fq -o 2M_barcode -q 20 -p TACC[4-7]AA[4-7]AA[4-7]TT[4-7]ATAA -m 2
+bartender_extractor_com -f 2M_test.fq -o 2M_barcode -q 63 -p TACC[4-7]AA[4-7]AA[4-7]TT[4-7]ATAA -m 2
