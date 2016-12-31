c58287750dc4e883f2cd10d854f00a4348e9aa4a
HAHA <luzhao1986@gmail.com>
2016-12-30 22:55:32 -0800

    fix the bundary issue when construct the center->cluster mapper by increasing the veter with size + 1.

--- src/centerclustermapper.cpp
+++ src/centerclustermapper.cpp
@@ -3,8 +3,8 @@
 #include <list>
 namespace barcodeSpace {
 CenterClusterMapper::CenterClusterMapper(int max_id)
-    : _cluster_2_center(max_id, std::list<std::string>()),
-      _cluster_2_instance(max_id, NULL)
+    : _cluster_2_center(max_id + 1, std::list<std::string>()),
+      _cluster_2_instance(max_id + 1, NULL)
 {
 }
 }
