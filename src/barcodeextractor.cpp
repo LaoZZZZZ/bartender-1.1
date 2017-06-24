4276e884f6ae3eb793d4cdfb0c8f0db5e15a3662
HAHA <luzhao1986@gmail.com>
2017-06-24 08:12:22 -0700

    correct the regular expression generation logic in bartender_extractor_com

--- src/barcodeextractor.cpp
+++ src/barcodeextractor.cpp
@@ -7,12 +7,14 @@
 #include <boost/regex.hpp>
 #include <string>
 using std::string;
+
 namespace barcodeSpace {
 
 bool BarcodeExtractor::ExtractBarcode(Sequence &read)  {
     int position = isMatched(read.fowardSeq(), read.quality());
     return position != -1;
 }
+    
 Sequence BarcodeExtractor::ExtractBarcode(const Sequence& read, bool& success) {
     success = false;
     Sequence result(read.id(), read.fowardSeq().c_str(), read.quality());
@@ -21,10 +23,10 @@ Sequence BarcodeExtractor::ExtractBarcode(const Sequence& read, bool& success) {
     return result;
 }
 
-int BarcodeExtractor::isMatched(string& sequence,string& qual){
+int BarcodeExtractor::isMatched(string& sequence, string& qual){
     boost::smatch result;
     // only consider full matched sequence
-    if(boost::regex_search(sequence, result, _pattern) && !result.empty() && result[0].matched){
+    if(boost::regex_search(sequence, result, _pattern, boost::match_flag_type::match_posix) && !result.empty() && result[0].matched){
         this->combinePieces(sequence,qual,result);
         return static_cast<int>(result.position());
     }
@@ -38,6 +40,7 @@ int BarcodeExtractor::isMatched(string& sequence,string& qual){
     }
     return -1;
 }
+    
 void BarcodeExtractor::combinePieces(string& sequence, string& qual, boost::smatch& result)  {
     assert(result.size() == _parts + 1);
     string temp;
@@ -50,7 +53,6 @@ void BarcodeExtractor::combinePieces(string& sequence, string& qual, boost::smat
     _total_bps += _suceeding.length();
     temp.clear();
     for (const auto& index : _random_part_index) {
-        //std::cout<< index << '\t' << result[index] << '\t' << result.position(index) << std::endl;
         temp += result[index];
         tempqual += qual.substr(result.position(index), result[index].length());
     }
