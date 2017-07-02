949e9ed5a091e0682f10a62142b1e5665ec1f6fd
HAHA <luzhao1986@gmail.com>
2017-07-02 10:14:59 -0700

    add barcode extractor unit test.

--- src/test/test_code/barcodeExtractor_unittest.cpp
+++ src/test/test_code/barcodeExtractor_unittest.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
 #include <cassert>
 #include <vector>
-#include "../../UmiExtractor.hpp"
+#include <boost/regex.hpp>
+#include <memory>
+#include <string>
+#include "../../barcodeextractor.h"
 #include "../../sequence.h"
 #include "gtest/gtest.h"
 
+using barcodeSpace::BarcodeExtractor;
+using barcodeSpace::Sequence;
+using boost::regex;
+using std::shared_ptr;
+using std::string;
+
 class BarcodeExtractorTest : public ::testing::Test {
-  private 
+protected:
+    std::shared_ptr<BarcodeExtractor> barcodeextractor;
+
+    virtual void SetUp() {
+        const size_t SPLITTED_PARTS = 3;
+        const string PATTERN = "(ACGT)(A|T|C|G|N){4,7}(TGCA)";
+        const string PRECEEDING = "ACGT";
+        const string SUCCEEDING = "TGCA";
+        const boost::regex pattern(PATTERN);
+        this->barcodeextractor.reset(new BarcodeExtractor(pattern, PRECEEDING, SUCCEEDING, SPLITTED_PARTS));
+    }
 };
+
+TEST_F(BarcodeExtractorTest, exactMatch) {
+    // sequence =CCG + ACGT(pre) + ACCTAGC + TGCA(succ)
+    // also use the sequence as quality sequence for convenience.
+    Sequence read("read_id", "CCGACGTACCTAGCTGCA", "CCGACGTACCTAGCTGCA");
+    ASSERT_EQ(barcodeSpace::FORWARD, barcodeextractor->ExtractBarcode(read));
+    Sequence expectedExtractedRead("read_id", "ACCTAGC", "ACCTAGC");
+    ASSERT_EQ(expectedExtractedRead, read);
+}
