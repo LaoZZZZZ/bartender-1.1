e79d0afdb4c7a91df1b54f600c77501147949cba
HAHA <luzhao1986@gmail.com>
2017-07-01 23:36:45 -0700

    add unit test for sequence.

---
+++ src/test/test_code/barcodeExtractor_unittest.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cassert>
+#include <vector>
+#include "../../UmiExtractor.hpp"
+#include "../../sequence.h"
+#include "gtest/gtest.h"
+
+class BarcodeExtractorTest : public ::testing::Test {
+  private 
+};
--- src/test/test_code/unit_test_runner.sh
+++ src/test/test_code/unit_test_runner.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 ./kmersDictionaryTest
 ./umiExtractorTest
+./sequenceTest
