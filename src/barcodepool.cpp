987e14cd64cf627a4d13ba78988acf4cb965f888
HAHA <luzhao1986@gmail.com>
2018-02-12 15:09:22 +0800

    remove dedup logic.

--- src/barcodepool.cpp
+++ src/barcodepool.cpp
@@ -27,7 +27,7 @@ BarcodePool::BarcodePool(std::unordered_map<std::string, std::vector<std::string
         _barcodes.push_back(b_freq.first);
         _total_raw_barcodes += b_freq.second.size();
         std::sort(b_freq.second.begin(), b_freq.second.end());
-        _primers.push_back(uniqueElement(b_freq.second));
+        _primers.push_back(b_freq.second);
         _num_replicates += b_freq.second.size() - _primers.back().size();
         _barcodeIndex.insert({b_freq.first, index});
         ++index;
